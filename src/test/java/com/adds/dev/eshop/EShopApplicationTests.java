8715afe31db588a29a78ffcb6081838cb47758e8
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-05-02 17:42:15 -0500

    Initial commit

---
+++ src/test/java/com/adds/dev/eshop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adds.dev.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
